4cd28f6e94eb580ae8708e7d1c19b40053b6dca2
Brenno Rodrigues <33203683+BRonen@users.noreply.github.com>
2020-01-09 15:06:23 -0300

    bug catch

--- src/controls/LinkController.js
+++ src/controls/LinkController.js
@@ -13,27 +13,26 @@ module.exports = {
     },
     async store(req, res) {
         const loggedUser = await User.findByIdAndUpdate(req.userId,
-        {$push: { posts: { name: req.body.name, content: req.body.url } }},
+        {$push: { links: { name: req.body.name, content: req.body.url } }},
         {safe: true, upsert: true, new: true, useFindAndModify: false});
         
         if(!loggedUser){
             return res.status(404).send('user not found');
         }
-        
-        console.log(loggedUser);
-        return res.json(loggedUser.posts);
+
+        return res.json(loggedUser.links);
     },
     async delete(req, res) {
 
-        const postId = mongoose.Types.ObjectId(req.body.postId);
+        const postId = mongoose.Types.ObjectId(req.body.linkId);
 
         const loggedUser = await User.findByIdAndUpdate(req.userId,
-            {$pull: { posts: { _id: postId } }},
+            {$pull: { links: { _id: linkId } }},
             {safe: true, upsert: true, new: true, useFindAndModify: false}
         );
         
         if(loggedUser){
-            return res.json(loggedUser.posts);
+            return res.json(loggedUser.links);
         }else{
             return res.status(404).send('you aren\'t the owner');
         }
